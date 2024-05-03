8884f5d8de8550fb559cd03547b0fa1376c42ab6
Melissa Autumn <melissa@thunderbird.net>
2024-05-03 08:52:27 -0700

    Revert "revert and log"
    This reverts commit c5ec069f6e3f1dab0b89e5c792b216c9ea54d091.

--- src/olympia/zadmin/views.py
+++ src/olympia/zadmin/views.py
@@ -304,9 +304,7 @@ def email_devs(request):
 
 @permission_required(amo.permissions.ANY_ADMIN)
 def index(request):
-    print(">> INDEX!")
     log = ActivityLog.objects.admin_events()[:5]
-    print(log)
     return render(request, 'zadmin/index.html', {'log': log})
 
 
